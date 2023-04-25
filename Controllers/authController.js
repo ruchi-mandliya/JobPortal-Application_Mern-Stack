9dbef2a10c062894b5fd1b2ee11b1cc1e1de6477
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-25 12:03:41 +0530

    generate token for login users

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -16,9 +16,16 @@ export const registerController = async (req, res, next) => {
     next("Email Already Register Please Login");
   }
   const user = await userModel.create({ name, email, password });
+  const token = user.createJWT();
   res.status(201).send({
     success: true,
     message: "User Created Successfully",
-    user,
+    user: {
+      name: user.name,
+      lastname: user.lastName,
+      email: user.email,
+      location: user.location,
+    },
+    token,
   });
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,6 +22,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: [6, "Password length should be greater than 6 characters"],
+      select: "true",
     },
     location: {
       type: String,
@@ -33,5 +35,11 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 export default mongoose.model("User", userSchema);
